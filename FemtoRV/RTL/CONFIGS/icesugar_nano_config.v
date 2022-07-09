33b221fc9faabc5b0ad66989398f6c632002d3f8
Pedro Starling Freitas <pedrostarling2000@gmail.com>
2022-07-09 00:55:09 -0300

    fixes icesugar nano synthesys

--- FemtoRV/RTL/CONFIGS/icesugar_nano_config.v
+++ FemtoRV/RTL/CONFIGS/icesugar_nano_config.v
@@ -2,11 +2,11 @@
 
 /************************* Devices **********************************************************************************/
 
-`define NRV_IO_LEDS          // Mapped IO, LEDs D1,D2,D3,D4 (D5 is used to display errors)
+//`define NRV_IO_LEDS          // Mapped IO, LEDs D1,D2,D3,D4 (D5 is used to display errors)
 //`define NRV_IO_IRDA          // In IO_LEDS, support for the IRDA on the IceSugar nano (WIP)
 //`define NRV_IO_UART          // Mapped IO, virtual UART (USB)
 `define NRV_IO_SSD1351       // Mapped IO, 128x128x64K OLED screen
-`define NRV_IO_MAX7219       // Mapped IO, 8x8 led matrix
+//`define NRV_IO_MAX7219       // Mapped IO, 8x8 led matrix
 `define NRV_MAPPED_SPI_FLASH // SPI flash mapped in address space. Can be used to run code from SPI flash.
 
 /************************* Processor configuration *******************************************************************/
@@ -24,8 +24,8 @@
 `define NRV_FEMTORV32_QUARK
 `define NRV_FREQ 36                 // Validated at 36 MHz on the IceSugar nano. Cannot overclock.
 `define NRV_RESET_ADDR 32'h00820000 // Jump execution to SPI Flash (800000h, +128k(20000h) for FPGA bitstream)
-`define NRV_COUNTER_WIDTH 24        // Number of bits in cycles counter
-`define NRV_TWOLEVEL_SHIFTER        // Faster shifts
+`define NRV_COUNTER_WIDTH 1        // Number of bits in cycles counter
+//`define NRV_TWOLEVEL_SHIFTER        // Faster shifts
 // tinyraytracer: 70 MHz, 17:30 
 
 
